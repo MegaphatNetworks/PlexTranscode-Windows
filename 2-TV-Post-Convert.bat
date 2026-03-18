65e46028921fbf2140f38ac4d39edaa8f98d012c
Megaphat Networks <60324301+MegaphatNetworks@users.noreply.github.com>
2026-03-18 16:14:53 -0400

    Modify TV post conversion script for HandBrakeCLI
    Updated HandBrakeCLI commands and added comments for usage instructions.

--- 2-TV-Post-Convert.bat
+++ 2-TV-Post-Convert.bat
@@ -1,4 +1,7 @@
 @echo off
-rem C:\HandBrakeCLI\HandBrakeCLI.exe -i %1 -o %1.mp4 -e x264 
-C:\HandBrakeCLI\HandBrakeCLI.exe -i %1 -o %1.mkv -e x264_10bit -f av_mkv
+REM Uncomment one of the following lines.  If you have a GPU, uncomment nvenc_h264 to use it, otherwise use av_mkv
+REM But DO NOT have BOTH HandbrakeCLI commands uncommented!!!
+REM Additionally I am including the first 3 streams (usually English, Spanish and English Subtitles in the U.S. ATSC streams)
+REM HandBrakeCLI.exe -e x264_10bit -f av_mkv -a "1,2,3" --all-subtitles -i %1 -o %1.mkv
+HandBrakeCLI.exe -e nvenc_h264 -f av_mkv -a "1,2,3" --all-subtitles -i %1 -o %1.mkv 
 rename %1 %1.old
